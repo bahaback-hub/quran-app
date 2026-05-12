291a20bc5dfed082571b9129149c1983b37fdff5
bahaback-hub <bahaback@gmail.com>
2026-05-12 18:13:22 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,71 @@
+const CACHE_NAME = 'quran-app-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('📦 جاري تخزين الملفات الأساسية');
+      return cache.addAll(STATIC_ASSETS);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          console.log('🗑️ حذف ذاكرة قديمة:', name);
+          return caches.delete(name);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var url = event.request.url;
+
+  if (event.request.method !== 'GET') return;
+
+  var isApiCall = url.indexOf('alquran.cloud') !== -1 ||
+                  url.indexOf('jsdelivr.net') !== -1 ||
+                  url.indexOf('everyayah.com') !== -1;
+
+  if (isApiCall) {
+    event.respondWith(
+      fetch(event.request)
+        .then(function(response) {
+          if (response.ok) {
+            var responseClone = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return response;
+        })
+        .catch(function() {
+          return caches.match(event.request);
+        })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request);
+    })
+  );
+});
